efe79a58a6d0b1af067570e0cfe8823486153dfb
Damien <damien.ramelet@protonmail.com>
2020-11-24 14:26:54 +0100

    Setup models and migrations

---
+++ contributions/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Comment(models.Model):
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    document = models.ForeignKey('documents.Document', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    changed_at = models.DateField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Commentaire'
+
+
+class Vote(models.Model):
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    document = models.ForeignKey('documents.Document', on_delete=models.CASCADE)
+    value = models.ForeignKey('contributions.VoteValue', on_delete=models.PROTECT)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+
+class VoteValue(models.Model):
+    value = models.CharField(max_length=128)
+
+    class Meta:
+        verbose_name = 'Types de vote'
+
+    def __str__(self):
+        return self.value
