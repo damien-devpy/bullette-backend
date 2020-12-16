199c3b363f01235ce66fd361e8da7c5a340aab0e
Damien <damien.ramelet@protonmail.com>
2020-12-16 10:38:58 +0100

    fix: route 'update_password' rename 'update_user'

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from .views import DeleteUserView, RegistrationView, UpdateUserView
 
 urlpatterns = [
     path("signup/", RegistrationView.as_view(), name="signup"),
-    path("update_password/", UpdateUserView.as_view(), name="update-password"),
+    path("update_user/", UpdateUserView.as_view(), name="update-user"),
     path("delete_account/", DeleteUserView.as_view(), name="delete-user"),
 ]
